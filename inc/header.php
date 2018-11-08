18756747fe03ee5ec4933ecb4efda7011871d814
BaptisteBobot <bobot.baptiste@gmail.com>
2018-11-08 12:17:51 +0100

    montravail

---
+++ inc/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="asset/css/style.css">
+  </head>
+  <body>
+    <div class="container">
+
+      <div class="wrap">
+
+        <div class="logo">
+          <a href="index.php"> <img src="asset\images\logo.png" alt=""></a>
+        </div>
+
+        <nav>
+
+          <ul>
+            <li> <a href="connexion.php">Login</a> </li>
+            <li> <a href="deconnexion.php">Logout</a> </li>
+            <li> <a href="inscription.php">Sign in</a> </li>
+          </ul>
+
+        </nav>
+
+      </div>
+    </div>
+  <div class="clear">
+
+  </div>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+include('inc/header.php');
+include('inc/function.php');
+include('inc/pdo.php');
+
+ ?>
+
+<?php
+$sql="SELECT * FROM movies_full ORDER BY rand() LIMIT 9";
+$query = $pdo -> prepare($sql);
+$query -> execute();
+$movies = $query -> fetchAll();
+?>
+
+<div class="wrap">
+
+
+<?php
+foreach ($movies as $movie) {
+  echo '<div class="film"><a href="detail.php?valeur='. $movie['id'] .'"><img src="posters/' . $movie['id'] . '.jpg" class="image" alt="' . $movie['title'] . '"></a>';
+  echo '<div class="titre">' . $movie['title'] . '</div>';
+  echo '<div class="year">' . $movie['year'] . '</div></div>';
+
+}
+?>
+</div>
+<div class="clear">
+
+</div>
