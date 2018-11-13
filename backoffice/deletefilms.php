382460b6648a82d1e5642005bb97539214700ec1
ConteAlexandre <TheLuffy@hotmail.fr>
2018-11-13 08:51:48 +0100

    test

--- backoffice/deletefilms.php
+++ backoffice/deletefilms.php
@@ -1,6 +1,6 @@
 <?php include('../inc/pdo.php'); ?>
 <?php include('../inc/function.php'); ?>
-<?php if(isadmin()){ ?>
+<!-- <?php if(isadmin()){ ?> -->
 
 <?php 
     if (!empty($_GET['id'])&&is_numeric($_GET['id'])) {
@@ -10,13 +10,14 @@
             $query = $pdo->prepare($sql);
             $query->bindValue(':id',$id, PDO::PARAM_STR);
             $query->execute();
-            header('Location: filmssupprimer.php');
+            // header('Location: filmssupprimer.php');
         }elseif (!empty($_POST['submittednon'])) {
-            header('Location: films.php');
+            // header('Location: films.php');
         }             
-    }else {
-        header('Location: ../404.php');
     }
+    // else {
+    //     header('Location: ../404.php');
+    // }
    
 ?>
 
@@ -25,12 +26,12 @@
 
 <p>Voulez vous supprimer ce film?</p>
 <form action="" method="post">
-<input type="submit" name="submittedoui" value="OUI" class="btn btn-outline">
+<input type="submit" name="submittedoui" value="OUI">
 <input type="submit" name="submittednon" value="NON">
 </form>
 
 
 <?php include('inc/footerb.php'); ?>
-<?php }else {
+<!-- <?php }else {
     die('403');
-}
+} ?>-->
